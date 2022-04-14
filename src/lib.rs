02747d571fe4b7fa95d87871fbed579198254c6c
yasuyuky <yasuyuki.ymd@gmail.com>
2022-04-14 19:56:29 +0900

    Add structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
 use proc_macro::TokenStream;
+use syn::{braced, parse::Parse, punctuated::Punctuated, token, Ident, Token, Type};
+
+struct Nestruct {
+    ident: Ident,
+    brace_token: token::Brace,
+    fields: Punctuated<NestableField, Token![,]>,
+}
+
+struct NestableField {
+    name: Ident,
+    colon_token: Token![:],
+    ty: FieldType,
+}
+
+enum FieldType {
+    Struct(Nestruct),
+    Type(Type),
+}
+
 
 #[proc_macro]
 pub fn nestruct(input: TokenStream) -> TokenStream {

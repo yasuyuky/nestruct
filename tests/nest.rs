612cf2d5df18e1356d0e4045b29951c7cbb0fcd1
yasuyuky <yasuyuki.ymd@gmail.com>
2022-05-06 10:56:58 +0900

    Add a test

---
+++ tests/nest.rs
@@ -0,0 +1,45 @@
+nestruct::nest! {
+    #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    FooBar {
+        foo: String?,
+        bar: [usize],
+        baz: {
+            qux: [{
+                quux: (usize, usize)?,
+                quuz: [String]?,
+            }],
+            corge: String,
+        },
+        grault: {
+            garply,
+            waldo,
+        }?,
+    }
+}
+
+#[test]
+fn test_nest_deserialize() {
+    let jsonstr = r#"
+    {
+        "foo": "foo",
+        "bar": [1, 2, 3],
+        "baz": {
+            "qux": [
+                {
+                    "quux": [1, 2],
+                    "quuz": "quuz"
+                },
+                {
+                    "quux": [3, 4],
+                    "quuz": "quuz2"
+                }
+            ],
+            "corge": "corge"
+        },
+        "grault": "garply"
+    }
+    "#;
+    let foobar = serde_json::from_str::<foo_bar::FooBar>(jsonstr).unwrap();
+    println!("{:#?}", foobar);
+}

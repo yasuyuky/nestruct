0c3d01c7a93e33158d7c9a83987cc2ca7e6cc3c2
yasuyuky <yasuyuki.ymd@gmail.com>
2022-06-16 23:28:56 +0900

    Add a test sample

---
+++ tests/flatten_complex.rs
@@ -0,0 +1,20 @@
+nestruct::flatten! {
+    FooBar {
+        /// foo
+        foo: String?,
+        /// bar
+        bar: [[usize]?]?,
+        baz: {
+            qux: [{
+                quux: (usize, usize)?,
+                quuz: String?,
+            }],
+            corge: String,
+        },
+        grault: {
+            garply,
+            waldo { wubble: String },
+            wubble(usize?, usize?)
+        }?,
+    }
+}

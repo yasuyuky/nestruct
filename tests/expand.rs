a726817641ec02d4005fc6c255d3654aff52ebc7
yasuyuky <yasuyuki.ymd@gmail.com>
2022-06-22 20:50:13 +0900

    Check status of cargo expand

--- tests/expand.rs
+++ tests/expand.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 fn check_expanded(test_name: &str) -> anyhow::Result<()> {
     let cargo_expand = Command::new("cargo").args(&["expand", "--test", test_name]).output()?;
+    assert!(cargo_expand.status.success());
     let expanded_str = String::from_utf8(cargo_expand.stdout)?;
     let should_be = std::fs::read_to_string(format!("tests/expanded/{test_name}.rs"))?;
     assert_eq!(expanded_str, should_be);

87d68690abf0eaca71afb96d6de1baf65980a9e0
yasuyuky <yasuyuki.ymd@gmail.com>
2022-06-23 20:14:14 +0900

    Update the expand test

--- tests/expand.rs
+++ tests/expand.rs
@@ -1,7 +1,8 @@
 use std::process::Command;
 
-fn check_expanded(test_name: &str) -> anyhow::Result<()> {
-    let cargo_expand = Command::new("cargo").args(&["expand", "--test", test_name]).output()?;
+fn check_expanded(test_name: &str, module_name: &str) -> anyhow::Result<()> {
+    let cargo_expand =
+        Command::new("cargo").args(&["expand", "--test", test_name, module_name]).output()?;
     assert!(cargo_expand.status.success());
     let expanded_str = String::from_utf8(cargo_expand.stdout)?;
     let should_be = std::fs::read_to_string(format!("tests/expanded/{test_name}.rs"))?;
@@ -11,7 +12,7 @@ fn check_expanded(test_name: &str) -> anyhow::Result<()> {
 
 #[test]
 fn test_expand() -> anyhow::Result<()> {
-    check_expanded("nest_complex")?;
-    check_expanded("flatten_complex")?;
+    check_expanded("nest_complex", "foo_bar")?;
+    check_expanded("flatten_complex", "foo_bar")?;
     Ok(())
 }

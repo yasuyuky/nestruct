de9ed81537ebf4a76a3d488f220e447800c46116
yasuyuky <yasuyuki.ymd@gmail.com>
2022-08-20 19:01:15 +0900

    Remove redundant to_string

--- src/lib.rs
+++ src/lib.rs
@@ -204,9 +204,9 @@ fn generate_fields<'a>(
 
 fn is_reset_attr(attr: &Attribute) -> bool {
     if let Some(ident) = attr.path.get_ident() {
-        if ident.to_string() == "nestruct" {
+        if ident == "nestruct" {
             if let Ok(arg) = attr.parse_args_with(Ident::parse_any) {
-                return arg.to_string() == "reset";
+                return arg == "reset";
             }
         }
     }
